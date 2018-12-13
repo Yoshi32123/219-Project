1aa9c83bbd0068379c21707930187c5f13b4887e
Kyler McQuillan (RIT Student) <kjm7088@ad.rit.edu>
2018-12-12 23:08:57 -0500

    added scripts to let dog walk around

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    // fields
+    public float radius;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Assets/Scripts/Pather.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+///     Pather Class:
+///     - Holds reference for the path and determines the ultimate force after implementing the seek for the current path index
+/// 
+/// </summary>
+
+public class Pather : Vehicle {
+
+    // ultimate force vector
+    public Vector3 ultimateForce;
+
+    // object references
+    public GameObject[] obstacles;
+    public GameObject[] path;
+
+    public int currentPathIndex = 0;
+
+    // Update is called once per frame
+    private new void Update()
+    {
+        base.Update();
+    }
+
+    /// <summary>
+    /// Overwriting calcsteeringforces to control forces
+    /// </summary>
+    public override void CalcSteeringForces()
+    {
+        // creating ultimate force vector
+        ultimateForce = Vector3.zero;
+
+        // seek current path index
+        ultimateForce += Seek(path[currentPathIndex].transform.position);
+
+        // if too close to current index, seek next one
+        if (CircleCollision(path[currentPathIndex], gameObject, 2))
+        {
+            // sets the next target
+            currentPathIndex++;
+
+            // prevents out of index error
+            if (currentPathIndex >= path.Length)
+            {
+                currentPathIndex = 0;
+            }
+        }
+
+        // scale ultimate force to max speed
+        ultimateForce = Vector3.ClampMagnitude(ultimateForce, maxSpeed);
+        ultimateForce.y = 0;
+
+        // apply ultimate force
+        ApplyForce(ultimateForce);
+    }
+}
